99de64f586b6f3b8bd0c5c65c4faf5ec96747a7c
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-08-05 12:11:30 -0700

    Implement adding history, use encryption

--- backend/index.js
+++ backend/index.js
@@ -43,10 +43,11 @@ app.get('/api/qrng', (req, res) => {
 });
 
 app.post('/api/users/add', (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  if (typeof name === 'string' && typeof email === 'string') {
-    dbConnection.query(`SELECT COUNT(Email) FROM Users WHERE Email="${email}";`,
+  const name = mysql.escape(req.body.name);
+  const email = mysql.escape(req.body.email);
+  const password = mysql.escape(req.body.password);
+  if (typeof name === 'string' && typeof email === 'string' && typeof password === 'string') {
+    dbConnection.query(`SELECT COUNT(Email) FROM Users WHERE Email=${email};`,
       (error, results, fields) => {
         if (error) {
           console.log(error);
@@ -54,7 +55,7 @@ app.post('/api/users/add', (req, res) => {
         } else {
           console.log(results);
           if (results[0]['COUNT(Email)'] === 0) {
-            dbConnection.query(`INSERT INTO Users (Name, Email) VALUES ("${name}", "${email}");`, 
+            dbConnection.query(`INSERT INTO Users (Name, Email, DecryptTest) VALUES (${name}, ${email}, AES_ENCRYPT('The quick brown fox jumped over the lazy dog.', UNHEX(SHA2(${password}, 256))));`, 
               (error, results, fields) => {
                 if (error) {
                   console.log(error);
@@ -73,7 +74,53 @@ app.post('/api/users/add', (req, res) => {
       });
     
   } else {
-    res.status(400).send('Expected JSON Request with "name" and "email" in body');
+    res.status(400).send('Expected JSON {name, email, password}');
+  }
+});
+
+app.post('/api/coinFlipHistory/add', (req, res) => {
+  const body = req.body;
+  const userID = body.userID;
+  const password = mysql.escape(body.password);
+  const dateTime = body.dateTime;
+  const outcome = mysql.escape(body.outcome);
+  const headsAction = mysql.escape(body.headsAction);
+  const tailsAction = mysql.escape(body.tailsAction);
+  if (Number.isInteger(userID) && typeof password === 'string' && Number.isInteger(dateTime) 
+    && typeof outcome === 'string' && typeof headsAction === 'string' && typeof tailsAction === 'string'
+  ) {
+    const selector = `AES_DECRYPT(DecryptTest, UNHEX(SHA2(${password}, 256)))`;
+    dbConnection.query(`SELECT ${selector} FROM Users WHERE ID=${userID};`,
+      (error, results, fields) => {
+        if (error) {
+          console.log(error);
+          res.status(500).send('Failed to check validity of password');
+        } else {
+          const testResult = results[0][selector];
+          console.log(testResult);
+          if (testResult && testResult.toString('utf-8') === 'The quick brown fox jumped over the lazy dog.') {
+            dbConnection.query(`INSERT INTO CoinFlipHistory (UserID, DateTime, Outcome, HeadsAction, TailsAction) `
+                            + `VALUES (${userID}, AES_ENCRYPT(${dateTime}, UNHEX(SHA2(${password}, 256))), AES_ENCRYPT(${outcome}, UNHEX(SHA2(${password}, 256))), `
+                            + `AES_ENCRYPT(${headsAction}, UNHEX(SHA2(${password}, 256))), AES_ENCRYPT(${tailsAction}, UNHEX(SHA2(${password}, 256))));`,
+              (error, results, fields) => {
+                if (error) {
+                  console.log(error);
+                  res.status(500).send('Failed to insert new history item');
+                } else {
+                  console.log(results);
+                  res.status(201).send('Success');
+                }
+              }
+            );
+          } else {
+            res.status(403).send('Wrong password');
+          }
+        }
+      }
+    );
+    
+  } else {
+    res.status(400).send('Expected JSON {userID, password, dateTime, outcome, headsAction, tailsAction}');
   }
 });
 
